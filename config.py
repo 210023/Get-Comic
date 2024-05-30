47189465023ec2b2e721485368443827e71977b5
210023 <3078622484@qq.com>
2024-05-31 02:33:16 +0800

    init commit of rust version

--- config.py
+++
@@ -1,7 +0,0 @@
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"
-}
-
-base_url = "https://nyahentai.biz"
-database = "./comic_set.yaml"
-log_base = "./logs"
